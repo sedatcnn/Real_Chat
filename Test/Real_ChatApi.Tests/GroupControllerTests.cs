2a8905d78d3209c2d84feebcee31c2d6e87c515d
sedatcnn <sedatcnr02@gmail.com>
2025-07-21 22:19:19 +0300

    SignalR UI entegrasyonu ve bug fixler

--- Test/Real_ChatApi.Tests/GroupControllerTests.cs
+++ Test/Real_ChatApi.Tests/GroupControllerTests.cs
@@ -1,32 +1,74 @@
-using Moq;
 using Xunit;
-using Real_ChatApi.webUI.Controllers;
-using System.Net.Http;
+using Moq;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+using System.Net.Http;
 using System.Net;
-using System.Net.Http.Headers;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Threading;
 using System.Text;
+using Real_ChatApi.webUI.Controllers;
 using Real_ChatApi.Dtos.GroupDtos;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Collections.Generic;
+using System;
+using Moq.Protected;
 
 public class GroupControllerTests
 {
     [Fact]
-    public async Task Create_ValidDto_ReturnsRedirect()
+    public async Task Create_Post_ValidDto_ReturnsRedirectToIndex()
     {
         // Arrange
-        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        var dto = new CreateGroupDto
+        {
+            Name = "Test Group",
+            IsPrivate = false,
+            InitialMemberUserIds = new List<Guid>()
+        };
+
+        // Mock HttpMessageHandler
         var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
 
-        var dto = new CreateGroupDto { Name = "Test Grup", IsPrivate = false, CreatedByUserId = Guid.NewGuid() };
+        handlerMock
+           .Protected()
+           .Setup<Task<HttpResponseMessage>>(
+              "SendAsync",
+              ItExpr.IsAny<HttpRequestMessage>(),
+              ItExpr.IsAny<CancellationToken>()
+           )
+           .ReturnsAsync(new HttpResponseMessage
+           {
+               StatusCode = HttpStatusCode.OK,
+               Content = new StringContent("true", Encoding.UTF8, "application/json"),
+           })
+           .Verifiable();
 
-        var responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
-        var httpClient = new HttpClient(new FakeHttpMessageHandler(responseMessage));
+        var httpClient = new HttpClient(handlerMock.Object)
+        {
+            BaseAddress = new Uri("http://real_chatapi.api:5000/")
+        };
 
-        httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+        // Mock IHttpClientFactory
+        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        httpClientFactoryMock
+            .Setup(_ => _.CreateClient(It.IsAny<string>()))
+            .Returns(httpClient);
 
+        // Mock HttpContext User claim for CreatedByUserId
         var controller = new GroupController(httpClientFactoryMock.Object);
+        var userId = Guid.NewGuid();
+
+        var claimsPrincipal = new System.Security.Claims.ClaimsPrincipal(
+            new System.Security.Claims.ClaimsIdentity(new[]
+            {
+                new System.Security.Claims.Claim(System.Security.Claims.ClaimTypes.NameIdentifier, userId.ToString())
+            })
+        );
+
+        controller.ControllerContext = new ControllerContext()
+        {
+            HttpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext { User = claimsPrincipal }
+        };
 
         // Act
         var result = await controller.Create(dto);
@@ -34,20 +76,12 @@ public class GroupControllerTests
         // Assert
         var redirectResult = Assert.IsType<RedirectToActionResult>(result);
         Assert.Equal("Index", redirectResult.ActionName);
-    }
-}
 
-public class FakeHttpMessageHandler : HttpMessageHandler
-{
-    private readonly HttpResponseMessage _response;
-
-    public FakeHttpMessageHandler(HttpResponseMessage response)
-    {
-        _response = response;
-    }
-
-    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
-    {
-        return Task.FromResult(_response);
+        // Verify HTTP call happened once
+        handlerMock.Protected().Verify(
+           "SendAsync",
+           Times.Once(),
+           ItExpr.IsAny<HttpRequestMessage>(),
+           ItExpr.IsAny<CancellationToken>());
     }
 }
